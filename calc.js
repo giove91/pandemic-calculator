6f2399d79dc5c36d02019a4d7727f8332821cdce
MarcoTrevisiol <marco.trevisiol@sns.it>
2018-02-12 21:11:52 +0100

    Aggiunta funzione di ripiego

--- calc.js
+++ calc.js
@@ -53,6 +53,16 @@ function State(cities) {
         this.infectRate.shift();
         this.isThereEpidemic = false;
     };
+    this.InitPlayerDeck = function (playerCards, epidemicCards) {
+        var stack;
+        this.playerDeck = [];
+        while (epidemicCards > 0) {
+            stack = Math.ceil(playerCards / epidemicCards);
+            this.playerDeck.push(stack + 1);
+            playerCards -= stack;
+            epidemicCards -= 1;
+        }
+    };
     this.GetStackIndexAndRemainder = function (cards) {
         var j = this.deck.length - 1;
         while (cards > this.deck[j].length && j >= 0) {
@@ -97,16 +107,6 @@ function State(cities) {
             return 0;
         return 1 / this.playerDeck[1];
     }
-    this.InitPlayerDeck = function (playerCards, epidemicCards) {
-        var stack;
-        this.playerDeck = [];
-        while (epidemicCards > 0) {
-            stack = Math.ceil(playerCards / epidemicCards);
-            this.playerDeck.push(stack + 1);
-            playerCards -= stack;
-            epidemicCards -= 1;
-        }
-    };
 }
 
 var saveKeys = ["deck", "discard", "cities", "playerDeck",
@@ -126,44 +126,53 @@ function LoadState(state, save) {
 
 var app = angular.module('pandemic', []);
 
+function InitData($scope,data) {
+    $scope.cities = data;
+    $scope.state = new State($scope.cities);
+    $scope.hist = [];
+    $scope.epidemic = false;
+    $scope.blockPlayerDeck = false;
+    $scope.ClickCity = function (city) {
+        $scope.blockPlayerDeck = true;
+        $scope.hist.push(SaveState($scope.state));
+        if ($scope.epidemic) {
+            $scope.state.Epidemic(city);
+            $scope.epidemic = false;
+        } else {
+            $scope.state.Infect(city);
+        }
+    };
+    $scope.GetGrid = function (turns, atleast, name) {
+        return ($scope.state.GetCityOdds(turns * $scope.state.infectRate[0], name, atleast)).toFixed(2)
+    }
+    $scope.orderParameter = 'name';
+    $scope.OrderTable = function (city) {
+        if (typeof $scope.orderParameter === 'object' && 'atleast' in $scope.orderParameter)
+            return [$scope.state.GetCityOdds($scope.orderParameter.turns * $scope.state.infectRate[0], city.name, $scope.orderParameter.atleast), city.name];
+        return [city.color, city.name];
+    }
+    $scope.infectrate = 2;
+    $scope.undo = function () {
+        LoadState($scope.state, $scope.hist.pop());
+    };
+    $scope.playercards = 66;
+    $scope.epidemiccards = 8;
+    $scope.updateplayerdeck = function () {
+        if ($scope.blockPlayerDeck) return;
+        $scope.state.InitPlayerDeck($scope.playercards, $scope.epidemiccards)
+    }
+    $scope.updateplayerdeck();
+}
+
+
 app.controller('calculator', function ($scope, $http) {
     $http.get('https://uz.sns.it/~giove/pandemic/deck/').then(
         function (response) {
-            $scope.cities = response.data;
-            $scope.state = new State($scope.cities);
-            $scope.hist = [];
-            $scope.epidemic = false;
-            $scope.blockPlayerDeck = false;
-            $scope.ClickCity = function (city) {
-                $scope.blockPlayerDeck = true;
-                $scope.hist.push(SaveState($scope.state));
-                if ($scope.epidemic) {
-                    $scope.state.Epidemic(city);
-                    $scope.epidemic = false;
-                } else {
-                    $scope.state.Infect(city);
-                }
-            };
-            $scope.GetGrid = function (turns, atleast, name) {
-                return ($scope.state.GetCityOdds(turns * $scope.state.infectRate[0], name, atleast)).toFixed(2)
-            }
-            $scope.orderParameter = 'name';
-            $scope.OrderTable = function (city) {
-                if (typeof $scope.orderParameter === 'object' && 'atleast' in $scope.orderParameter)
-                    return [$scope.state.GetCityOdds($scope.orderParameter.turns * $scope.state.infectRate[0], city.name, $scope.orderParameter.atleast), city.name];
-                return [city.color, city.name];
-            }
-            $scope.infectrate = 2;
-            $scope.undo = function () {
-                LoadState($scope.state, $scope.hist.pop());
-            };
-            $scope.playercards = 66;
-            $scope.epidemiccards = 8;
-            $scope.updateplayerdeck = function () {
-                if ($scope.blockPlayerDeck) return;
-                $scope.state.InitPlayerDeck($scope.playercards, $scope.epidemiccards)
-            }
-            $scope.updateplayerdeck();
+            InitData($scope, response.data);
+        }
+    ).catch(
+        function () {
+            InitData($scope,[{"color": "blue", "multiplicity": 3, "name": "New York"}, {"color": "blue", "multiplicity": 2, "name": "Paris"}, {"color": "blue", "multiplicity": 2, "name": "San Francisco"}, {"color": "blue", "multiplicity": 3, "name": "Washington"}, {"color": "black", "multiplicity": 3, "name": "Cairo"}, {"color": "black", "multiplicity": 3, "name": "Istanbul"}, {"color": "black", "multiplicity": 3, "name": "Tripoli"}, {"color": "yellow", "multiplicity": 2, "name": "Bogot\u00e0"}]);
         }
     );
 });
